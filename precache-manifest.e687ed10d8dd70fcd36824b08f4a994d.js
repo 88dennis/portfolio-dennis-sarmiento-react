943dc0e05fdba57088565bdf71ba20f7d91a600c
Dennis <dennissarmiento808@gmail.com>
2020-04-25 14:18:38 -0700

    Updates

---
+++ precache-manifest.e687ed10d8dd70fcd36824b08f4a994d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475d6b657806c2a5a10d2259089de9c3",
+    "url": "/portfolio-dennis-sarmiento-react/index.html"
+  },
+  {
+    "revision": "d2d8cf6e708c5cfb1d3d",
+    "url": "/portfolio-dennis-sarmiento-react/static/css/main.79b5a4d7.chunk.css"
+  },
+  {
+    "revision": "d00873d517ac4ca22c4a",
+    "url": "/portfolio-dennis-sarmiento-react/static/js/2.b1ca71c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-dennis-sarmiento-react/static/js/2.b1ca71c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d8cf6e708c5cfb1d3d",
+    "url": "/portfolio-dennis-sarmiento-react/static/js/main.7b67fa4a.chunk.js"
+  },
+  {
+    "revision": "4a1fac78036368106803",
+    "url": "/portfolio-dennis-sarmiento-react/static/js/runtime-main.cb7e8d7f.js"
+  },
+  {
+    "revision": "6f69d94ae9e8ec66d3ec6a0f2f476a51",
+    "url": "/portfolio-dennis-sarmiento-react/static/media/DS3.6f69d94a.png"
+  },
+  {
+    "revision": "aee0f87f19a0b29206e529199c9f6f8b",
+    "url": "/portfolio-dennis-sarmiento-react/static/media/DS3NOBGBG.aee0f87f.png"
+  }
+]);
